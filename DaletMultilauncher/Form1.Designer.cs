40395275481bd92c2fb2799b2d334ef07d403034
Дмитрий Воронов <dim.voron.00@yandex.ru>
2023-08-14 16:58:54 +0300

    Добавьте файлы проекта.

---
+++ DaletMultilauncher/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿using System.Drawing;
+
+namespace DaletMultilauncher
+{
+    partial class DaletMultilauncher
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DaletMultilauncher));
+            this.reserveDalet = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.mainDalet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // reserveDalet
+            // 
+            this.reserveDalet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.reserveDalet.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.reserveDalet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.reserveDalet.Location = new System.Drawing.Point(300, 228);
+            this.reserveDalet.Name = "reserveDalet";
+            this.reserveDalet.Size = new System.Drawing.Size(192, 64);
+            this.reserveDalet.TabIndex = 0;
+            this.reserveDalet.Text = "Reserve Dalet";
+            this.reserveDalet.UseVisualStyleBackColor = false;
+            this.reserveDalet.Click += new System.EventHandler(this.btnRDaletClick);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCancel.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnCancel.Location = new System.Drawing.Point(300, 312);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(192, 64);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // mainDalet
+            // 
+            this.mainDalet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.mainDalet.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mainDalet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.mainDalet.Location = new System.Drawing.Point(300, 144);
+            this.mainDalet.Name = "mainDalet";
+            this.mainDalet.Size = new System.Drawing.Size(192, 64);
+            this.mainDalet.TabIndex = 4;
+            this.mainDalet.Text = "Dalet";
+            this.mainDalet.UseVisualStyleBackColor = false;
+            this.mainDalet.Click += new System.EventHandler(this.btnMDaletClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(391, 355);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "label1";
+            // 
+            // DaletMultilauncher
+            // 
+            this.AccessibleName = "DaletMultilauncher";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DaletMultilauncher.Properties.Resources.Background;
+            this.ClientSize = new System.Drawing.Size(790, 517);
+            this.Controls.Add(this.mainDalet);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.reserveDalet);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(806, 556);
+            this.MinimumSize = new System.Drawing.Size(806, 556);
+            this.Name = "DaletMultilauncher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DaletMultilauncher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button reserveDalet;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button mainDalet;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DaletMultilauncher/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+namespace DaletMultilauncher
+{
+    public partial class DaletMultilauncher : Form
+    {
+        public const UInt32 Infinite = 0xffffffff;
+        public const Int32 Startf_UseStdHandles = 0x00000100;
+        public const Int32 StdOutputHandle = -11;
+        public const Int32 StdErrorHandle = -12;
+
+        public String fromDaletLanToRttv = @"net use \\xx.xx.xx.xx\test\test\Temp\scripts_domain /user:rttv.ru\login password" +
+               Environment.NewLine +
+               @"Copy-Item '\\xx.xx.xx.xx\test\test\Temp\scripts_domain\fullReturnChangeDomain.ps1' -Destination 'C:\Temp'" +
+               Environment.NewLine + "set-executionpolicy unrestricted" + Environment.NewLine + @"C:\Temp\fullReturnChangeDomain.ps1";
+        public String fromRttvToDaletLan = @"net use \\xx.xx.xx.xx\test\test\Temp\scripts_domain /user:rttv.ru\login password" +
+               Environment.NewLine +
+               @"Copy-Item '\\xx.xx.xx.xx\test\test\Temp\scripts_domain\fullChangeDomain.ps1' -Destination 'C:\Temp'" +
+               Environment.NewLine + "set-executionpolicy unrestricted" + Environment.NewLine + @"C:\Temp\fullChangeDomain.ps1";
+
+
+
+        public DaletMultilauncher()
+        {
+            InitializeComponent();
+            ActiveControl = label1;
+        }
+
+        public void execution(String dalet)
+        {
+             
+            if (dalet.Equals("Dalet"))
+            {
+                try
+                {
+                    
+                    changeDomain(fromDaletLanToRttv);
+                    
+                }
+                catch (Exception er)
+                {
+                    Console.WriteLine("Exception: " + er.Message);
+                    
+                }
+                finally
+                {
+                    Console.WriteLine("Error");
+                }
+            }    
+                
+            if (dalet.Equals("Reserve Dalet")) 
+            {
+                try
+                {
+                    changeDomain(fromRttvToDaletLan);
+                    
+                }
+                catch (Exception er)
+                {
+                    Console.WriteLine("Exception: " + er.Message);
+                    
+                }
+                finally
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+
+        public void changeDomain(String changeDomain)
+        {
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo("powershell.exe");
+            p.StartInfo.Arguments = changeDomain;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.Start();
+
+            
+
+        }
+
+
+        private void btnMDaletClick(object sender, EventArgs e)
+        {
+            execution("Dalet");
+            Close();
+            
+        }
+
+        private void btnRDaletClick(object sender, EventArgs e)
+        {
+            execution("Reserve Dalet");
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
+
+
+/*Process[] processDalet = Process.GetProcessesByName("DaletGalaxy");
+
+if (processDalet.Length > 0)
+{
+    foreach (Process p in processDalet)
+    {
+        p.Kill();
+    }
+}
+
+Process.Start("\"C:\\\\Program Files\\\\Dalet\\\\DaletPlus\\\\bin\\\\DaletGalaxy.exe\"");*/
